6643f2b768169e9330761e162a299d4acbc856dd
Tiago <tiago.m.gameiro@gmail.com>
2025-05-20 17:38:10 +0200

    Reducer+ SignUpLink

--- reducers/user.js
+++ reducers/user.js
@@ -1,7 +1,19 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-	value: { user: null }, // Valeur initiale du reducer
+	profile: {
+  name: null,
+  firstName: null,
+  email: null,
+  phoneNumber: null,
+  address: [{streetNumber: null,
+	streetName: null,
+	city: null,
+	zipCode: null
+  }
+
+  ],
+} // Valeur initiale du reducer
 };
 
 export const userSlice = createSlice({
@@ -9,14 +21,17 @@ export const userSlice = createSlice({
 	initialState,
 	// Fonctions à importer dans les composants pour agir sur le reducer
 	reducers: {
-		connectUser: (state, action) => {
-			state.value.uid = action.payload;
-		},
-		disconnectUser: (state) => {
-			state.value.user = null;
-		},
+		// connectUser: (state, action) => {
+		// 	state.value.uid = action.payload;
+		// },
+		// disconnectUser: (state) => {
+		// 	state.value.user = null;
+		
+		updateEmail: (state,action) => {
+     state.profile.email = action.payload;
+   },
 	},
 });
 
-export const { connectUser, disconnectUser } = userSlice.actions;
+export const {updateEmail } = userSlice.actions;
 export default userSlice.reducer;
