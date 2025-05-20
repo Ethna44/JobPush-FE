c35ed7c6844d26cbf7f44f0b65f595394a52ad79
Your nameMarion <marion.rdx@gmail.com>
2025-05-20 11:59:01 +0200

    AppStyles creation for global style & style Accueil

---
+++ AppStyles.js
@@ -0,0 +1,31 @@
+export default AppStyles = {
+    color: {
+        background: '#F9F1F1',
+        font: '#2B3033',
+        accent: "#F72C03",
+        cards: "#F3E4E5"
+    },
+    title: {
+        fontFamily: 'Poppins',
+        fontSize: 36,
+        fontWeight: "semi-bold",
+        marginBottom: 20,
+    },
+    button : {
+        alignItems: 'center',
+        justifyContent : 'center' ,
+        paddingTop: 10,
+        paddingBottom: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+        backgroundColor: '#F72C03',
+        borderRadius: 10,
+    },
+    buttonText : {
+        alignItems: 'center',
+        justifyContent : 'center' ,
+        color: '#F9F1F1',
+        fontFamily: "Poppins",
+        fontSize: 14,
+    }
+}
--- screens/Acceuil.jsx
+++ screens/Acceuil.jsx
@@ -1,49 +1,78 @@
-import { StyleSheet, Text, View, Button, TouchableOpacity } from "react-native";
+import { StyleSheet, Text, View, Button, TouchableOpacity, Image } from "react-native";
+import AppStyles from "../AppStyles";
 
 export default function StackScreen1({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Bienvenue !</Text>
-      <TouchableOpacity onPress={() => navigation.navigate("Inscription")} style={styles.button}>
-        <Text style={styles.text}>S'inscrire</Text>
-      </TouchableOpacity>
-
-      <Text>OU</Text>
-     <TouchableOpacity onPress={() => navigation.navigate("Connexion")} style={styles.button}>
-        <Text style={styles.text}>Se Connecter</Text>
-      </TouchableOpacity>
-      <Text>S'identifier avec google</Text>
-      <Button title="Google" onPress={() => navigation.navigate("Connexion")}  />
+      <View style={styles.imageContainer}>
+        <Image style={styles.logo} source={require('../assets/logoJobPush-Photoroom.jpg')}></Image>
+      </View>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Bienvenue !</Text>
+      </View>
+      <View style={styles.redButtonContainer}>
+        <TouchableOpacity onPress={() => navigation.navigate("Inscription")} style={styles.button}>
+          <Text style={styles.buttonText}>S'INSCRIRE</Text>
+        </TouchableOpacity>
+        <Text>OU</Text>
+        <TouchableOpacity onPress={() => navigation.navigate("Connexion")} style={styles.button}>
+          <Text style={styles.buttonText}>SE CONNECTER</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.googleContainer}>
+        <Text>S'identifier avec</Text>
+        <Button title="Google" onPress={() => navigation.navigate("Connexion")}  />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // Add your styles here
-  // Example:
   container: {
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
-	backgroundColor:"#F9F1F1"
+    backgroundColor: AppStyles.color.background,
+    // borderColor: "red",
+    // borderWidth: 1,
   },
-  title: {
-    fontSize: 30,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  button:{
-	height:50,
-	width:200,
-	justifyContent: "center",
+  imageContainer: {
+    width: '100%',
+    justifyContent: "center",
     alignItems: "center",
-	color:"#F9F1F1",
-	backgroundColor:"#F72C03",
-	margin:40
+    // borderColor: "black",
+    // borderWidth: 1,
   },
-text:{
-	color:"#F9F1F1",
-	fontWeight:600,
-	fontSize:20
-}
+  logo: {
+    width: 100,
+    height: 100,
+    margin: 10,
+  },
+  titleContainer: {
+    width: '100%',
+    justifyContent: "center",
+    alignItems: "center",
+    // borderColor: "blue",
+    // borderWidth: 1,
+  },
+  title: AppStyles.title,
+  redButtonContainer: {
+    width: '100%',
+    height: '30%',
+    justifyContent: "space-between",
+    padding: 40,
+    alignItems: "center",
+    // borderColor: "green",
+    // borderWidth: 1,
+  },
+  button: AppStyles.button,
+  buttonText: AppStyles.buttonText,
+  googleContainer: {
+    width: '100%',
+    justifyContent: "space-between",
+    //padding: 40,
+    alignItems: "center",
+    // borderColor: "orange",
+    // borderWidth: 1,
+  }
 });
