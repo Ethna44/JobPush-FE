9dbacd4f13bb55089d37da7a64f5af817a544ff7
Your nameMarion <marion.rdx@gmail.com>
2025-05-20 10:18:56 +0200

    safeareaview on header

--- components/Header.js
+++ components/Header.js
@@ -1,24 +1,22 @@
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, SafeAreaView } from "react-native";
 import React from 'react'
 
 export default function Header() {
   return (
-    <View style={styles.header}>
-      <Text style={{color:'#F9F1F1',fontSize:24}}>Job<Text style={{color:'#F72C03'}}>Push</Text></Text>
-    </View>
+    <SafeAreaView style={styles.header}>
+        <Text style={{color:'#F9F1F1',fontSize:24}}>Job<Text style={{color:'#F72C03'}}>Push</Text></Text>
+    </SafeAreaView>
+    
   )
 }
 
 const styles = StyleSheet.create({
-    // Add your styles here
-    // Example:
     header: {
         height:'8%',
         width:"100%",
         backgroundColor:'#2B3033',
-        justifyContent:'center',
+        justifyContent:'flex-end',
+        paddingBottom: 5,
         alignItems:'center',
-      
     },
-
 });
