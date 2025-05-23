4c5de8e68b2c2c8afca521d033e6b067f1545b0f
Tiago <tiago.m.gameiro@gmail.com>
2025-05-23 17:41:40 +0200

    fav Dynamic Done & JobCard CSS

--- screens/Offres.jsx
+++ screens/Offres.jsx
@@ -14,6 +14,7 @@ import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useState, useRef, useEffect } from "react";
 import JobCard from "../components/Jobcard";
 import { callOffresApi, reverseGeocode } from "../apiUtilis";
+import AppStyles from "../AppStyles";
 
 export default function TabScreen1({ navigation }) {
   const [search, setSearch] = useState("");
@@ -23,39 +24,38 @@ export default function TabScreen1({ navigation }) {
   const clientSecret = process.env.EXPO_PUBLIC_CLIENT_SECRET_FT;
   const tokenManagerRef = useRef(null);
 
-//   Job title = motsCles
-// sector = grandDomaine = code Domaine (json sector)
-// typeContract = typeContrat
-// remote= null
-// city = commune = code insee (json cities)
-// region = region = code region (json regions)
-
+  //   Job title = motsCles
+  // sector = grandDomaine = code Domaine (json sector)
+  // typeContract = typeContrat
+  // remote= null
+  // city = commune = code insee (json cities)
+  // region = region = code region (json regions)
 
   if (!tokenManagerRef.current) {
     tokenManagerRef.current = new TokenManager(clientId, clientSecret);
   }
-    
- useEffect(() => {
+
+  useEffect(() => {
     fetch(`${EXPO_IP}/offers`)
       .then((response) => response.json())
       .then((data) => {
         console.log(data);
-        setOffersData(data.offers)
+        setOffersData(data.offers);
       })
-      .then(() =>   callOffresApi(tokenManagerRef.current))
+      .then(() => callOffresApi(tokenManagerRef.current));
   }, []);
   // console.log(offersData)
-  
-   const offer = offersData.map((data, i) => {
+
+  const offer = offersData.map((data, i) => {
     // console.log(offer)
-   
-    return <JobCard key={i} {...data}  />;
+
+    return <JobCard key={i} {...data} />;
   });
 
   return (
     <SafeAreaView style={styles.container}>
-     <ScrollView contentContainerStyle={styles.scrollView}>
-      <View style={styles.container}>
+      <View style={styles.topContainer}>
+        <Text style={styles.title}>Offres d'emploi</Text>
         <View style={styles.inputSearchContainer}>
           <TextInput
             placeholder="Recherche"
@@ -65,19 +65,9 @@ export default function TabScreen1({ navigation }) {
           />
           <FontAwesome name={"search"} size={18} color="#F72C03" />
         </View>
-        <Text style={styles.title}>Offres</Text>
-
-        <Button
-          title="Go to StackScreen1"
-          onPress={() => navigation.navigate("Accueil")}
-        />
       </View>
-      <View style={styles.jobContainer}>
-       {offer}
-      </View>
-
-      
-        
+      <ScrollView contentContainerStyle={styles.scrollView}>
+        <View style={styles.card}>{offer}</View>
       </ScrollView>
     </SafeAreaView>
   );
@@ -85,93 +75,40 @@ export default function TabScreen1({ navigation }) {
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: "flex-start",
+    flex: 1,
+    // borderColor: "green",
+    // borderWidth: 1,
+  },
+
+  title: AppStyles.title,
+
+  topContainer: {
+    justifyContent: "center",
     alignItems: "center",
-  },
-  title: {
-    fontSize: 20,
-    fontWeight: "bold",
-    marginBottom: 20,
-  },
-  inputContainer: {
-    flexDirection: "row",
-    justifyContent: "space-between",
-    alignItems: "center",
-    width: "80%",
-    backgroundColor: "#ffffff",
-    padding: 20,
-    marginTop: 20,
-    borderRadius: 10,
-  },
-  input: {
-    width: "65%",
-    marginTop: 6,
-    borderBottomColor: "#ec6e5b",
-    borderBottomWidth: 1,
-    fontSize: 16,
-  },
-  button: {
-    width: "30%",
-    alignItems: "center",
-    paddingTop: 8,
-    backgroundColor: "#ec6e5b",
-    borderRadius: 10,
-  },
-  textButton: {
-    color: "#ffffff",
-    height: 24,
-    fontWeight: "600",
-    fontSize: 15,
+    width: "100%",
+    // borderColor: "pink",
+    // borderWidth: 1,
   },
   inputSearch: {
     flex: 1,
     marginTop: 6,
-
     fontSize: 17,
-    paddingRight: 8,
+    paddingBottom: 8,
   },
   inputSearchContainer: {
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
-    width: "75%",
-
-    borderBottomColor: "black",
+    width: "80%",
+    borderBottomColor: "#2B3033",
     borderBottomWidth: 1,
-
     marginTop: 5,
   },
-  card: {
-    backgroundColor: "#F3E4E5",
-    borderRadius: 12,
-    width: "85%",
-    height: "25%",
-    flexDirection: "row",
-    padding: 12,
-    alignItems: "center",
+  scrollView: {
+    // borderColor: "blue",
+    // borderWidth: 1,
+    width: "100%",
+    alignItems: 'center',
+    paddingVertical: 20,
   },
-  logo: {
-    width: 60,
-    height: 60,
-    resizeMode: "cover",
-    borderRadius: 8,
-    marginRight: 10,
-  },
-  source: {
-    fontWeight: "bold",
-  },
-  textInfo: {
-    fontSize: 16,
-    fontStyle: "italic",
-  },
-  info: {
-    flex: 1,
-    gap: 3,
-  },
-  jobContainer:{
-    height:"100%"
-  },
-  // scrollView : {
-  //   border
-  // }
 });
