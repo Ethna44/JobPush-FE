282452d59cf0d2ff63e53c3e813cdfd11f3e0ca9
Ethan <ethan.frappie@gmail.com>
2025-05-28 11:42:43 +0200

    todo and notes

--- screens/Connexion.jsx
+++ screens/Connexion.jsx
@@ -113,6 +113,7 @@ export default function LogIn({ navigation }) {
       <View style={styles.buttonAndTextContainer}>
         <TouchableOpacity
           onPress={() => {
+            navigation.navigate("TabNavigator")
             handleRegister();
           }}
           style={styles.button}
